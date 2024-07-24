1391047ca4ad9c9da29e32d19a99e4eae9b54863
TheArthurVincent <arthurcardosocorp@gmail.com>
2024-07-24 19:17:58 -0300

    *

--- server/routes/homeworkRoutes/homeworkRoutes.js
+++ server/routes/homeworkRoutes/homeworkRoutes.js
@@ -1,11 +1,27 @@
-const { homework_getAll } = require("../../controller/homeworkController/getAllHomework/getAllHomework");
-const { homework_done } = require("../../controller/homeworkController/homeworkDone/homeworkDone");
-const { loggedIn, loggedInADM } = require("../../controller/studentsController/loggedInAuth/loggedInAuth");
+const {
+  homework_getAll,
+} = require("../../controller/homeworkController/getAllHomework/getAllHomework");
+const {
+  homework_done,
+} = require("../../controller/homeworkController/homeworkDone/homeworkDone");
+const {
+  loggedIn,
+  loggedInADM,
+} = require("../../controller/studentsController/loggedInAuth/loggedInAuth");
 
 const homeworkRoutes = [
-    { method: 'get', path: '/homework/:id', middlewares: [loggedIn], handler: homework_getAll },
-    { method: 'put', path: '/homework/:id', middlewares: [loggedIn, loggedInADM], handler: homework_done }
+  {
+    method: "get",
+    path: "/homework/:id",
+    middlewares: [loggedIn],
+    handler: homework_getAll,
+  },
+  {
+    method: "put",
+    path: "/homework/:id",
+    middlewares: [loggedIn, loggedInADM],
+    handler: homework_done,
+  },
 ];
 
-
-module.exports = { homeworkRoutes }
+module.exports = { homeworkRoutes };
